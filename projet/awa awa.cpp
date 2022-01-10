f47203857ef4b7fa670d87dcf59f78d5e3e39a90
Lilian Ouraha <lilianouraha@hotmail.fr>
2022-01-10 16:31:10 +0100

    message

--- projet/awa awa.cpp
+++ projet/awa awa.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
+void fonction(int& x) {
+	x -= 2;
+}
+
 int main() {
 	cout << "uwu";
+
+	int i = 48;
+
+	fonction(i);
+
+	cout << i << endl;
+
+
+
 	return 0;
 }
+
